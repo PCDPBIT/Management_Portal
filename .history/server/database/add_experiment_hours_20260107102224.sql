7475fa22beebdca424113c2dc18c01735f342fb9
AshwinSasikumar <ashwinsasikumar@Ashwins-MacBook-Pro.local>
2026-01-07 16:28:23 +0530

    Clean up .history files and update Docker configuration

--- .history/server/database/add_experiment_hours_20260107102224.sql
+++
@@ -1,3 +0,0 @@
--- Add hours column to course_experiments table
-ALTER TABLE course_experiments 
-ADD COLUMN hours INT DEFAULT 0 AFTER experiment_name;
--- .history/server/handlers/regulation_20251227154556.go
+++
@@ -1,4 +0,0 @@
-package handlers
-
-// This file contains legacy curriculum handlers
-// All functions have been moved to curriculum.go to avoid conflicts with new regulation system
